a407988824a481d84185a37370ce5033ce43c23d
Marlon Hamaekers <marlon@marlon-m3-max.home>
2023-12-28 21:07:09 +0100

    .

--- src/test/java/CustomerTestDrive.java
+++ src/test/java/CustomerTestDrive.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class CustomerTestDrive {
+import be.nextlevelclinic.calculator.Calculator;
+import be.nextlevelclinic.customer.Customer;
+
+public class CustomerTestDrive {
+    public static void main(String[] args) {
+        Customer customer1 = new Customer(1.85, 99);
+        Customer customer2 = new Customer();
+        System.out.println(customer1);
+        System.out.println(customer2);
+        System.out.println(Customer.getCustomers());
+        System.out.println(Customer.getCustomers());
+
+    }
 }
